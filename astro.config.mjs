22966ac2aab56ba5165847e620ca9aa79567b188
BraynSramos <braynramos97@gmail.com>
2025-10-22 16:37:47 -0500

    Actualizar configuración de Astro para incluir el sitio y la base correctos

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,8 +5,8 @@ import react from "@astrojs/react";//Dependecia de React-👾
 // https://astro.build/config
 export default defineConfig({
     // Cambia USERNAME y REPO_NAME por los tuyos
-  site: 'https://brayanSramos.github.io/',
-  base: '/', // Asegura rutas limpias (sin /Mi-Porfolio/)
+  site: 'https://brayanSramos.github.io/Profile/',
+  base: '/Profile/', // Asegura rutas limpias (sin /Mi-Porfolio/)
   output: 'static',
   integrations: [tailwind(), //Configuracion de Tailwind-👾
   react(),
